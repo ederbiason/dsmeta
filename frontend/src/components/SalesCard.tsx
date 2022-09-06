955e2e46813b144cc3fc6aff676c63972a1fe4c6
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-06 15:27:17 -0300

    Date picker
    Installed the date picker tool to use the component to get the date
    
    And setted some funcionality for the input tag

--- frontend/src/components/SalesCard.tsx
+++ frontend/src/components/SalesCard.tsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
 
 export const SalesCard = () => {
+    const min = new Date(new Date().setDate(new Date().getDate() - 365))
+    const max = new Date()
+
+    const [minDate, setMinDate] = useState(min)
+    const [maxDate, setMaxDate] = useState(max)
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={minDate}
+                        onChange={(date: Date) => { setMinDate(date) }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
                 <div className="dsmeta-form-control-container">
-                    <input className="dsmeta-form-control" type="text" />
+                    <DatePicker
+                        selected={maxDate}
+                        onChange={(date: Date) => { setMaxDate(date) }}
+                        className="dsmeta-form-control"
+                        dateFormat="dd/MM/yyyy"
+                    />
                 </div>
             </div>
 
