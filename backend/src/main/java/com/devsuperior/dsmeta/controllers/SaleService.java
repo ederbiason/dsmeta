9312512eab951f13462c8e51283d97160f6eeb64
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-09-08 14:44:33 -0300

    API Test
    Created the service, controller and repository and tested the API with Rapid API

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleService.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import com.devsuperior.dsmeta.entities.Sale;
+
+public class SaleService {
+
+    public List<Sale> findSales() {
+        return null;
+    }
+
+}
